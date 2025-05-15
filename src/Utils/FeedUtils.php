f5cda1a7bc7d494a84ce78c13e3bcd6eccedf15f
Franco-JG <jeftefranco@outlook.com>
2025-05-15 16:24:06 -0600

    chore: se eliminó la tabulación extra que causaba error con declaración del namespace

--- src/Utils/FeedUtils.php
+++ src/Utils/FeedUtils.php
@@ -1,19 +1,18 @@
-    <?php
+<?php
+namespace OAW\Backend\Utils;
+use SimplePie\SimplePie;
 
-    namespace OAW\Backend\Utils;
-    use SimplePie\SimplePie;
-
-    function getFeedNameFromUrl($url) {
-        $feed = new SimplePie();
-        $feed->enable_cache(false);
-        $feed->set_feed_url($url);
-        $feed->init();
-        // Manejar errores
-        if ($feed->error()) {
-            echo 'Error al cargar el feed: ' . $feed->error()."\n";
-            exit;
-        }
-        return $feed->get_title();
+function getFeedNameFromUrl($url) {
+    $feed = new SimplePie();
+    $feed->enable_cache(false);
+    $feed->set_feed_url($url);
+    $feed->init();
+    // Manejar errores
+    if ($feed->error()) {
+        echo 'Error al cargar el feed: ' . $feed->error()."\n";
+        exit;
     }
+    return $feed->get_title();
+}
 
-    ?>
+?>
